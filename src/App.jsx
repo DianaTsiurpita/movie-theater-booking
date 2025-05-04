736f0e80b9c466e925ebdf268c76875e780bf0e7
DianaTsiurpita <DIANA.TSIURPITA@lnu.edu.ua>
2025-05-04 18:18:38 +0300

    перехід до лаб.9 створено  CinemaHall.jsx  Home.jsx ooking.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from 'react';
 import MovieList from './components/MovieList';
 import movies from './data/movies';
@@ -7,7 +6,18 @@ import './index.css';
 const App = () => {
     return (
       <div>
+        {/* Основний контент (список фільмів) */}
         <MovieList movies={movies} />
+
+        {/* Контактна інформація та місце знаходження */}
+        <footer className="footer">
+          <div className="footer-content">
+            <p>Контакти:</p>
+            <p>Телефон: +38 099 123 4567</p>
+            <p>Email: info@kino.com</p>
+            <p>Адреса: вул. Кінотеатральна, 45, Київ, Україна</p>
+          </div>
+        </footer>
       </div>
     );
 };
